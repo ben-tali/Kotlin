256b29f7a727a858ef847dcdfa0081227d5b2356
Baraka Mulumia <barakamulumia@gmail.com>
2021-06-28 05:46:30 +0300

    Update main.kt
    Access [Visibilty] Modifiers OverView

--- src/main/kotlin/io/baraka/main.kt
+++ src/main/kotlin/io/baraka/main.kt
@@ -5,3 +5,11 @@ fun main() {
     jay.printInfo()
 }
 
+/*
+access modifiers
+in kotlin everything (visibility) in general is public by default
+public
+internal (public within the module)
+private (available within the file of implementation)
+protected (available within the class/subclass)
+ */
